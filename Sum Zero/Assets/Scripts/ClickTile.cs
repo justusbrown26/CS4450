a7a1affff89d9cbad68c45c195232e03348a8235
Justus Brown <justusbrown@mail.weber.edu>
2018-01-23 20:13:50 -0700

    Working version of the game, still in development

---
+++ Sum Zero/Assets/Scripts/ClickTile.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTile : MonoBehaviour {
+
+    static public GameObject frameRef;
+
+    public Sprite tileGliph;
+    public Sprite tileOpen;
+    public bool isOpen = true;
+    public int myId = 0;
+    public int myNumber = 0;
+    private bool mouseIsOver = false;
+
+    private void Start()
+    {
+        FrameManager.tileArray[myId] = gameObject;
+        CloseTile();
+        myNumber = Random.Range(-9, 10);
+    }
+
+    private void Update()
+    {
+        if (mouseIsOver)
+        {
+            if (Input.GetMouseButtonDown(0)) // if tile is clicked
+            {
+                if (isOpen) // if tile is already open
+                {
+                    CloseTile();
+                }
+                else
+                {
+                    OpenTile();
+                }
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                CloseTile();
+            }
+        }
+
+    }
+
+    void OnMouseEnter()
+    {
+        mouseIsOver = true;
+    }
+
+    void OnMouseExit()
+    {
+        mouseIsOver = false;
+    }
+
+    public void OpenTile()
+    {
+        GetComponent<SpriteRenderer>().sprite = tileOpen;
+        GetComponentInChildren<TextMesh>().text = "" + myNumber;
+        isOpen = true;
+        frameRef.GetComponent<FrameManager>().PlayTile(myId, myNumber); // plays the tile, counts the sum
+    }
+
+    public void CloseTile()
+    {
+        GetComponent<SpriteRenderer>().sprite = tileGliph;
+        GetComponentInChildren<TextMesh>().text = "";
+        isOpen = false;
+    }
+
+}
---
+++ Sum Zero/Assets/Scripts/FrameManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameManager : MonoBehaviour {
+
+	static public GameObject[] tileArray = new GameObject[8];
+	private bool gameStarted = false;
+	private int countOpen, sum;
+	private int[] chosenTile = new int[] { -1, -1, -1 };
+	private int timeLeftToClose = 100;
+	private const int CLOSE_TIME = 100;
+
+	// Use this for initialization
+	void Start () {
+		ClickTile.frameRef = gameObject; // gives the ClickTile class a reference to the frame
+		countOpen = 0;
+		sum = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameStarted)
+		{
+			gameStarted = true;
+			MakeSolution();
+		}
+		else
+		{
+			if(countOpen == 3)
+			{
+				if (timeLeftToClose == 0)
+				{
+					CloseOpenTiles();
+				}
+				else
+				{
+					timeLeftToClose--;
+				}
+			}
+		}
+	}
+
+	// Makes sure that the game has at least one solution
+
+	void MakeSolution()
+	{
+
+		List<int> randomTiles = new List<int>();
+		int num = 0;
+		while (randomTiles.Count < 3)
+		{
+			num = Random.Range(0, 8);
+			if (!randomTiles.Contains(num))
+			{
+				randomTiles.Add(num);
+			}
+		}
+
+		int num1 = tileArray[randomTiles[0]].GetComponent<ClickTile>().myNumber;
+		int num2 = tileArray[randomTiles[1]].GetComponent<ClickTile>().myNumber;
+
+		// Make sure we don't end up with a number less than -9
+		if (num1 + num2 > 9)
+		{
+			num1 -= num1 + num2 - 9;
+			tileArray[randomTiles[0]].GetComponent<ClickTile>().myNumber = num1;
+		}
+
+		// Make sure we don't end up with a number larger than 9
+		if (num1 + num2 < -9)
+		{
+			num1 -= num1 + num2 + 9;
+			tileArray[randomTiles[0]].GetComponent<ClickTile>().myNumber = num1;
+		}
+
+		tileArray[randomTiles[2]].GetComponent<ClickTile>().myNumber = -(num1 + num2);
+
+		Debug.Log("tile1 = " + randomTiles[0] + " tile2 = " + randomTiles[1] + " tile3 = " + randomTiles[2]);
+	}
+
+	// when a tile is clicked on
+	public void PlayTile(int id, int number)
+	{
+		if(countOpen == 3)
+		{
+			FrameManager.tileArray[id].GetComponent<ClickTile>().CloseTile();
+			return;
+		}
+		chosenTile[countOpen] = id;
+		timeLeftToClose = CLOSE_TIME;
+		sum += number;
+		countOpen++;
+		if(countOpen == 3)
+		{
+			if(sum == 0)
+			{
+				GameWon();
+			}
+		}
+		Debug.Log("Clicked tile: " + id + " with number: " + number);
+	}
+
+	public void CloseOpenTiles()
+	{
+		for (int i = 0; i < countOpen; i++)
+		{
+			foreach (var tile in chosenTile)
+			{
+				tileArray[tile].GetComponent<ClickTile>().CloseTile();
+			}
+			sum = 0;
+			countOpen = 0;
+			timeLeftToClose = CLOSE_TIME;
+		}
+	}
+
+	private void GameWon()
+	{
+		foreach (var tile in tileArray)
+		{
+			tile.GetComponent<ClickTile>().gameObject.SetActive(false);
+		}
+		gameObject.SetActive(false);
+		
+	}
+}
